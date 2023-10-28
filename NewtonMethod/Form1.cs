fa293b36eb3186da315a4956a6c52b87ef543586
Anastasia <anastasiya.reksius@mail.ru>
2023-10-28 10:43:51 +0700

    ??

--- NewtonMethod/Form1.cs
+++ NewtonMethod/Form1.cs
@@ -54,29 +54,37 @@ namespace NewtonMethod
 
                     return;
                 }
-                Func<double, double> function = x => 10 * x - 10;
-                Func<double, double> derivative = x => 10;
+                Func<double, double> function = x => (27 - 18 * x + 2 * Math.Pow(x, 2)) * Math.Exp(-(x / 3));
+                Func<double, double> derivative = x => (4*x-18)*Math.Exp(-(x/3)) - ((2 * Math.Pow(x, 2) - 18 * x + 27) * Math.Exp(-(x / 3)))/3;
 
                 double x0 = (a + b) / 2;
                 double x1 = x0;
-                do
-                {
-                    x0 = x1;
-                    x1 = x0 - (function(x0) / derivative(x0));
-                } while (Math.Abs(function(x0) / derivative(x0)) >= exp);
-                textBox4.Text = x1.ToString();
-                chart1.Series.Clear();
-                Series series = new Series("Function");
-                series.ChartType = SeriesChartType.Line;
+                
 
-                for (double x = a; x <= b; x += 0.1)
+                if (function(a) * function(b) <= 0)
                 {
-                    double y = function(x);
-                    series.Points.AddXY(x, y);
+                    do
+                    {
+                        x0 = x1;
+                        x1 = x0 - (function(x0) / derivative(x0));
+                    } while (Math.Abs(function(x0) / derivative(x0)) >= exp);
+                    textBox4.Text = $"{x1}:F4";//x1.ToString();
+                    chart1.Series.Clear();
+                    Series series = new Series("Function");
+                    series.ChartType = SeriesChartType.Line;
+
+                    for (double x = a; x <= b; x += 0.1)
+                    {
+                        double y = function(x);
+                        series.Points.AddXY(x, y);
+                    }
+
+                    chart1.Series.Add(series);
+                } else
+                {
+                    MessageBox.Show("Отсутствуют корни на данном отрезке");
                 }
-
-                chart1.Series.Add(series);
-
+                               
             }
             catch (Exception ex)
             {
