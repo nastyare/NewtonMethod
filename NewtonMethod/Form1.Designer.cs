184c4f53d85f0b275487aa5f61217fcd8040e931
Anastasia <anastasiya.reksius@mail.ru>
2023-11-10 21:03:51 +0700

    wanna be your setting lotion

--- NewtonMethod/Form1.Designer.cs
+++ NewtonMethod/Form1.Designer.cs
@@ -30,10 +30,12 @@
         {
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.menuStrip3 = new System.Windows.Forms.MenuStrip();
             this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.calculateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.goldenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -43,6 +45,12 @@
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.function = new System.Windows.Forms.Label();
             this.menuStrip3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
@@ -55,7 +63,7 @@
             this.menuToolStripMenuItem});
             this.menuStrip3.Location = new System.Drawing.Point(0, 0);
             this.menuStrip3.Name = "menuStrip3";
-            this.menuStrip3.Size = new System.Drawing.Size(750, 29);
+            this.menuStrip3.Size = new System.Drawing.Size(1000, 29);
             this.menuStrip3.TabIndex = 7;
             this.menuStrip3.Text = "menuStrip3";
             // 
@@ -63,7 +71,8 @@
             // 
             this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.calculateToolStripMenuItem,
-            this.deleteToolStripMenuItem});
+            this.deleteToolStripMenuItem,
+            this.goldenToolStripMenuItem});
             this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
             this.menuToolStripMenuItem.Size = new System.Drawing.Size(64, 25);
             this.menuToolStripMenuItem.Text = "Menu";
@@ -71,21 +80,28 @@
             // calculateToolStripMenuItem
             // 
             this.calculateToolStripMenuItem.Name = "calculateToolStripMenuItem";
-            this.calculateToolStripMenuItem.Size = new System.Drawing.Size(176, 30);
-            this.calculateToolStripMenuItem.Text = "Рассчитать";
+            this.calculateToolStripMenuItem.Size = new System.Drawing.Size(218, 30);
+            this.calculateToolStripMenuItem.Text = "Метод Ньютона";
             this.calculateToolStripMenuItem.Click += new System.EventHandler(this.calculateToolStripMenuItem_Click);
             // 
             // deleteToolStripMenuItem
             // 
             this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
-            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(176, 30);
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(218, 30);
             this.deleteToolStripMenuItem.Text = "Очистить";
             this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // goldenToolStripMenuItem
+            // 
+            this.goldenToolStripMenuItem.Name = "goldenToolStripMenuItem";
+            this.goldenToolStripMenuItem.Size = new System.Drawing.Size(218, 30);
+            this.goldenToolStripMenuItem.Text = "Золотое сечение";
+            this.goldenToolStripMenuItem.Click += new System.EventHandler(this.goldenToolStripMenuItem_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(44, 46);
+            this.label1.Location = new System.Drawing.Point(23, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(25, 16);
             this.label1.TabIndex = 1;
@@ -94,7 +110,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(277, 46);
+            this.label2.Location = new System.Drawing.Point(207, 43);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(25, 16);
             this.label2.TabIndex = 2;
@@ -103,7 +119,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(528, 46);
+            this.label3.Location = new System.Drawing.Point(383, 46);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(25, 16);
             this.label3.TabIndex = 3;
@@ -111,21 +127,21 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(77, 43);
+            this.textBox1.Location = new System.Drawing.Point(65, 37);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(317, 46);
+            this.textBox2.Location = new System.Drawing.Point(250, 43);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 5;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(569, 46);
+            this.textBox3.Location = new System.Drawing.Point(431, 46);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 22);
             this.textBox3.TabIndex = 6;
@@ -134,47 +150,103 @@
             // 
             chartArea1.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea1);
-            this.chart1.Location = new System.Drawing.Point(60, 146);
+            this.chart1.Location = new System.Drawing.Point(12, 141);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(619, 304);
+            this.chart1.Size = new System.Drawing.Size(952, 376);
             this.chart1.TabIndex = 8;
             this.chart1.Text = "chart1";
+            title1.Name = "Title1";
+            this.chart1.Titles.Add(title1);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(211, 97);
+            this.textBox4.Location = new System.Drawing.Point(92, 90);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
-            this.textBox4.TabIndex = 9;
+            this.textBox4.Size = new System.Drawing.Size(149, 22);
+            this.textBox4.TabIndex = 10;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(47, 100);
+            this.label4.Location = new System.Drawing.Point(23, 93);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(144, 16);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "Локальный минимум:";
+            this.label4.Size = new System.Drawing.Size(57, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Корень:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(328, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "минимум:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(625, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "максимум:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(413, 96);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(149, 22);
+            this.textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(725, 102);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(149, 22);
+            this.textBox6.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(626, 49);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(362, 22);
+            this.textBox7.TabIndex = 16;
+            // 
+            // function
+            // 
+            this.function.AutoSize = true;
+            this.function.Location = new System.Drawing.Point(567, 49);
+            this.function.Name = "function";
+            this.function.Size = new System.Drawing.Size(42, 16);
+            this.function.TabIndex = 17;
+            this.function.Text = "F(x) = ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(750, 450);
+            this.ClientSize = new System.Drawing.Size(1000, 529);
+            this.Controls.Add(this.function);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.menuStrip3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip3);
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip3.ResumeLayout(false);
@@ -186,19 +258,28 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.MenuStrip menuStrip3;
         private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem calculateToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolStripMenuItem goldenToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label function;
     }
 }
-
--- NewtonMethod/Form1.cs
+++ NewtonMethod/Form1.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.DataVisualization.Charting;
+using org.matheval;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.Differentiation;
+using System.Net;
+using MathNet.Numerics.Differentiation;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace NewtonMethod
@@ -19,86 +16,272 @@ namespace NewtonMethod
         {
             InitializeComponent();
         }
-
+        double F(double X)
+        {
+            org.matheval.Expression expression = new org.matheval.Expression(textBox7.Text.ToLower());
+            expression.Bind("x", X);
+            decimal value = expression.Eval<decimal>();
+            return (double)value;
+        }
         private void calculateToolStripMenuItem_Click(object sender, EventArgs e)
         {
             try
             {
-                double a, b, exp;
-
-                if (!double.TryParse(textBox1.Text, out a))
+                double a, b, Xi;
+                if (!double.TryParse(textBox1.Text, out a) || !double.TryParse(textBox2.Text, out b) || !double.TryParse(textBox3.Text, out Xi))
                 {
-                    MessageBox.Show("В а должно быть только натуральное число.");
-
-                    return;
+                    throw new ArgumentException("Некорректные значения входных данных");
                 }
-
-                if (!double.TryParse(textBox2.Text, out b))
+                if (a >= b)
                 {
-                    MessageBox.Show("В b должно быть только натуральное число.");
-
-                    return;
+                    throw new ArgumentException("Некорректные границы интервала");
                 }
-
-                if (a > b)
+                this.chart1.Series[0].Points.Clear();
+                double x = a;
+                double y;
+                while (x <= b)
                 {
-                    MessageBox.Show("a не может быть больше b");
-
-                    return;
+                    y = F(x);
+                    this.chart1.Series[0].Points.AddXY(x, y);
+                    x += 0.1;
                 }
-
-                if (!double.TryParse(textBox3.Text, out exp) || !Regex.IsMatch(textBox3.Text, @"(1|10+)|(0,(1|0+1))")
-                    || textBox3.Text[0] == '-')
+                Xi = (int)-Math.Log10(Xi);
+                double Root;
+                if (F(a) * F(b) <= 0)
                 {
-                    MessageBox.Show("неправильный формат. пример: 0,1");
+                    Root = (a + b) / 2;
 
-                    return;
-                }
-                Func<double, double> function = x => (27 - 18 * x + 2 * Math.Pow(x, 2)) * Math.Exp(-(x / 3));
-                Func<double, double> derivative = x => (4*x-18)*Math.Exp(-(x/3)) - ((2 * Math.Pow(x, 2) - 18 * x + 27) * Math.Exp(-(x / 3)))/3;
-
-                double x0 = (a + b) / 2;
-                double x1 = x0;
-                
-
-                if (function(a) * function(b) <= 0)
-                {
-                    do
+                    while (Math.Abs(b - a) > Math.Pow(10, -Xi))
                     {
-                        x0 = x1;
-                        x1 = x0 - (function(x0) / derivative(x0));
-                    } while (Math.Abs(function(x0) / derivative(x0)) >= exp);
-                    textBox4.Text = $"{x1}:F4";//x1.ToString();
-                    chart1.Series.Clear();
-                    Series series = new Series("Function");
-                    series.ChartType = SeriesChartType.Line;
+                        double y1 = F(a), y2 = F(b), y3 = F(Root);
+                        if (y1 * y3 < 0)
+                        {
+                            b = Root;
+                        }
+                        else if (y2 * y3 < 0)
+                        {
+                            a = Root;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        Root = (a + b) / 2;
 
-                    for (double x = a; x <= b; x += 0.1)
-                    {
-                        double y = function(x);
-                        series.Points.AddXY(x, y);
                     }
-
-                    chart1.Series.Add(series);
-                } else
-                {
-                    MessageBox.Show("Отсутствуют корни на данном отрезке");
+                    //if ((27 - 18 * Root + 2 * Math.Pow(Root, 2)) * Math.Exp(-Root / 3) < 0 + Root && (27 - 18 * Root + 2 * Math.Pow(Root, 2)) * Math.Exp(-Root / 3) > 0 - Root)
+                    //{
+                    textBox4.Text = $"{Root:F4}";
+                    //}
                 }
-                               
+                else
+                {
+                    throw new ArgumentException("Нет корней на этом интервале или их больше одного");
+                }
+                if (!double.TryParse(textBox1.Text, out a) || !double.TryParse(textBox2.Text, out b) || !double.TryParse(textBox3.Text, out Xi))
+                {
+                    throw new ArgumentException("Некорректные значения входных данных");
+                }
+                double max, min;
+                double delta = Xi / 10;
+                while (b - a >= Xi)
+                {
+                    double middle = (a + b) / 2;
+                    double lambda = middle - delta, mu = middle + delta;
+                    if (F(lambda) < F(mu))
+                        b = mu;
+                    else
+                        a = lambda;
+                }
+                min = (a + b) / 2;
+
+
+                // Точка максимума
+                if (!double.TryParse(textBox1.Text, out a) || !double.TryParse(textBox2.Text, out b) || !double.TryParse(textBox3.Text, out Xi))
+                {
+                    throw new ArgumentException("Некорректные значения входных данных");
+                }
+                while (b - a >= Xi)
+                {
+                    double middle = (a + b) / 2;
+                    double lambda = middle - delta, mu = middle + delta;
+                    if (F(lambda) > F(mu))
+                        b = mu;
+                    else
+                        a = lambda;
+                }
+                max = (a + b) / 2;
+
+                textBox5.Text = $"{min:F4}";
+                textBox6.Text = $"{max:F4}";
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Что-то пошло не так");
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+        public void GoldenSectionSearchMin(Func<double, double> f, double StartPoint, double EndPoint, double epsilon)
+        {
+            double x1, x2, k1, k2, F1, F2, Result;
+            int count = 0;
+            k2 = (Math.Sqrt(5) - 1) / 2;
+            k1 = 1 - k2;
+            x1 = StartPoint + k1 * (EndPoint - StartPoint);
+            x2 = StartPoint + k2 * (EndPoint - StartPoint);
+            try
+            {
+                F1 = F(x1);
+                F2 = F(x2);
+                while (true)
+                {
+                    ++count;
+                    if ((EndPoint - StartPoint) < epsilon)
+                    {
+                        Result = (StartPoint + EndPoint) / 2;
+                        textBox5.Text = $"{Result:F4}";
+                        break;
+                    }
+                    else
+                    {
+                        if (F1 < F2)
+                        {
+                            EndPoint = x2;
+                            x2 = x1;
+                            F2 = F1;
+                            x1 = StartPoint + k1 * (EndPoint - StartPoint);
+                            F1 = F(x1);
+                        }
+                        else
+                        {
+                            StartPoint = x1;
+                            x1 = x2;
+                            F2 = F1;
+                            x2 = StartPoint + k2 * (EndPoint - StartPoint);
+                            F2 = F(x2);
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+        public void GoldenSectionSearchMax(Func<double, double> f, double StartPoint, double EndPoint, double epsilon)
+        {
+            double x1, x2, k1, k2, F1, F2, Result;
+            int count = 0;
+            k2 = (Math.Sqrt(5) - 1) / 2;
+            k1 = 1 - k2;
+            x1 = StartPoint + k1 * (EndPoint - StartPoint);
+            x2 = StartPoint + k2 * (EndPoint - StartPoint);
+            try
+            {
+                F1 = F(x1);
+                F2 = F(x2);
+                while (true)
+                {
+                    ++count;
+                    if ((EndPoint - StartPoint) < epsilon)
+                    {
+                        Result = (StartPoint + EndPoint) / 2;
+                        textBox6.Text = $"{Result:F4}";
+                        break;
+                    }
+                    else
+                    {
+                        if (F1 > F2)
+                        {
+                            EndPoint = x2;
+                            x2 = x1;
+                            F2 = F1;
+                            x1 = StartPoint + k1 * (EndPoint - StartPoint);
+                            F1 = F(x1);
+                        }
+                        else
+                        {
+                            StartPoint = x1;
+                            x1 = x2;
+                            F2 = F1;
+                            x2 = StartPoint + k2 * (EndPoint - StartPoint);
+                            F2 = F(x2);
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
             }
         }
 
+        public double MethodOfNewton(double a, double b, double accuracy)
+        {
+
+            double x0 = (a + b) / 2;
+
+            double x1 = x0;
+
+            //if (F(a) * F(b) <= accuracy)
+            //{
+            do
+            {
+                x0 = x1;
+                x1 = x0 - (F(x0) / Differentiate.FirstDerivative(F, x0));
+            } while (Math.Abs(F(x0) / Differentiate.FirstDerivative(F, x0)) >= accuracy);
+            //} else
+            //{
+
+            //}
+
+            return x1;
+
+        }
         private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
         {
             textBox1.Clear();
             textBox2.Clear();
             textBox3.Clear();
-            chart1.Series[0].Points.Clear();
-            //textBox1.Text = string.Empty;
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+        }
+        private void goldenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a, b, Xi;
+                if (!double.TryParse(textBox1.Text, out a) || !double.TryParse(textBox2.Text, out b) || !double.TryParse(textBox3.Text, out Xi))
+                {
+                    throw new ArgumentException("Некорректные значения входных данных");
+                }
+                if (a >= b)
+                {
+                    throw new ArgumentException("Некорректные границы интервала");
+                }
+                this.chart1.Series[0].Points.Clear();
+                double x = a;
+                double y;
+                while (x <= b)
+                {
+                    y = F(x);
+                    this.chart1.Series[0].Points.AddXY(x, y);
+                    x += 0.1;
+                }
+
+                GoldenSectionSearchMax(F, a, b, Xi);
+                GoldenSectionSearchMin(F, a, b, Xi);
+                double root = MethodOfNewton(a, b, Xi = (int)-Math.Log10(Xi));
+                textBox4.Text = $"{root:F4}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+
         }
     }
 }
